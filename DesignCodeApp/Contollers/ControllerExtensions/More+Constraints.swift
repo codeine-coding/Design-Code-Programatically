138e21ecbacb8736a22cc041cc97fe9f105e5b94
Allen Whearry <allen@codeinecoding.com>
2018-08-25 10:39:03 -0400

    add extension for MoreViewController constraints

---
+++ DesignCodeApp/Contollers/ControllerExtensions/More+Constraints.swift
@@ -0,0 +1,23 @@
+//
+//  More+Constraints.swift
+//  DesignCodeApp
+//
+//  Created by Allen Whearry on 8/25/18.
+//  Copyright © 2018 Allen Whearry. All rights reserved.
+//
+
+import UIKit
+
+extension MoreViewController {
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+    }
+    
+    
+    private func displayConstraints() {
+        
+    }
+    
+}
