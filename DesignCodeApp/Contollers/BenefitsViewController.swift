9c1d4d82dcb12542a13b9cd988f438cb278bca09
Allen Whearry <allen@codeinecoding.com>
2018-09-14 21:47:40 -0400

    create benefits view controller

---
+++ DesignCodeApp/Contollers/BenefitsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BenefitsViewController.swift
+//  DesignCodeApp
+//
+//  Created by Allen Whearry on 9/14/18.
+//  Copyright © 2018 Allen Whearry. All rights reserved.
+//
+
+import UIKit
+
+class BenefitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+    
+    private func setupView() {
+        
+    }
+
+}
